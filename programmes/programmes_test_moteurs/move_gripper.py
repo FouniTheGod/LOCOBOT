ddbffff347098d4f3132f2119e400d1f4f433f79
PierreFarret <81699879+PierreFarret@users.noreply.github.com>
2022-03-18 08:24:39 +0100

    Amélioration de la lisibilité du code

--- programmes/programmes_test_moteurs/move_gripper.py
+++ programmes/programmes_test_moteurs/move_gripper.py
@@ -7,11 +7,11 @@ from interbotix_xs_modules.locobot import InterbotixLocobotXS
 def main():
     locobot = InterbotixLocobotXS(robot_model="locobot_wx200", arm_model="mobile_wx200")
     
-    pressure = float(input("Pressure (de 0 jusqu'à 3.68 exclu) ? "))
+    pressure = float(input("Pression de la pince (de 0 jusqu'à 3.68 exclu) ? "))
     
+    # Ouverture, puis fermeture de la pince avec la pression saisie
     locobot.gripper.open()
     locobot.gripper.set_pressure(pressure)
-    
     locobot.gripper.close()
     
 if __name__=='__main__':
