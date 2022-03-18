843f6ab7b1d3cb628c198c542d01fc3b6537868e
PierreFarret <81699879+PierreFarret@users.noreply.github.com>
2022-03-18 08:15:27 +0100

    Amélioration du code

--- programmes/programmes_test_moteurs/move_base.py
+++ programmes/programmes_test_moteurs/move_base.py
@@ -1,19 +1,40 @@
 from interbotix_xs_modules.locobot import InterbotixLocobotXS
 
-# This script commands the base to move to an arbitrary point on a map
-# Note that this script assumes you already have a map built
-#
 # To get started, open a terminal and type...
 # 'roslaunch interbotix_xslocobot_control xslocobot_python.launch robot_model:=locobot_wx200 use_nav:=true localization:=true'
 # Then change to this directory and type 'python move_base.py'
 
+def setMenu(texte, choixMin, choixMax):
+    """Génère un menu à partir d'un texte et d'un intervalle  de valeurs pour le choix"""
+    
+    while choix < choixMin || choixMax < choix:
+        choix = int(input(MENU_ARTICULATION))
+        
+        if choix < choixMin || choixMax < choix:
+            print("Le choix doit être compris entre " + str(choixMin) + " et " + str(choixMax))
+            
+    return choix
+
+
+def toDegreePerSecond(radPerSecond):
+    """Convertie les rad/s en deg/s"""
+    return radPerSecond * 180 / math.pi
+
+
 def main():
+    MENU_ANGLE = "Unité de vitesse angulaire:\n -1 Rad/s\n -2 Deg/s"
+    
     locobot = InterbotixLocobotXS(robot_model="locobot_wx200", arm_model="mobile_wx200", use_move_base_action=True)
 
     translation = float(input("Vitesse (en m/s) ? "))
-    rotation = float(input("Vitesse de rotation (en rad/s) ? "))
+    unit = setMenu(MENU_ANGLE, 1, 2)
+    rotation = float(input("Vitesse de rotation ? "))
     time = float(input("Temps d'exécution (en s) ? "))
-
+    
+    if unit == 2:
+        rotation = toDegreePerSecond(angle)
+    
+    # Effectue le déplacement
     locobot.base.move(translation, rotation, time)
 
 if __name__=='__main__':
