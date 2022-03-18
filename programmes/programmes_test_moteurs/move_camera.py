e5d53d3f165ef34522ca72c699e6fffbaf5c932e
PierreFarret <81699879+PierreFarret@users.noreply.github.com>
2022-03-18 08:21:53 +0100

    Amélioration de la lisibilité du code

--- programmes/programmes_test_moteurs/move_camera.py
+++ programmes/programmes_test_moteurs/move_camera.py
@@ -3,15 +3,38 @@ from math import pi
 
 # 'roslaunch interbotix_xslocobot_control xslocobot_python.launch robot_model:=locobot_wx200'
 
-def to_degree(deg):
-    return deg * pi / 180
+def setMenu(texte, choixMin, choixMax):
+    """Génère un menu à partir d'un texte et d'un intervalle  de valeurs pour le choix"""
+    
+    while choix < choixMin || choixMax < choix:
+        choix = int(input(MENU_ARTICULATION))
+        
+        if choix < choixMin || choixMax < choix:
+            print("Le choix doit être compris entre " + str(choixMin) + " et " + str(choixMax))
+            
+    return choix
+
+
+def toDegree(rad):
+    """Convertie les radians en degrés"""
+    return rad * 180 / math.pi
+
 
 def main():
-    locobot = InterbotixLocobotXS(robot_model="locobot_wx200", arm_model="mobile_wx200") #use_move_base_action=True)
+    MENU_ARTICULATION = "Choisissez une articulation:\n 1- Pan\n 2- Tilt"
+    MENU_ANGLE = "Unité d'angle:\n -1 Radian\n -2 Degré"
     
-    joint_name = str(input("Joint name ? [pan, tilt] "))
-    angle = float(input("Angle (rad) ? "))
+    locobot = InterbotixLocobotXS(robot_model="locobot_wx200", arm_model="mobile_wx200")
     
+    joint = setMenu(MENU_ARTICULATION, 1, 2)
+    unit = setMenu(MENU_ANGLE, 1, 2)
+    
+    angle = float(input("Angle ? "))
+    
+    if unit == 2:
+        angle = toDegree(angle)
+    
+    # Remise de la caméra à sa position de base
     locobot.camera.pan_tilt_go_home()
     
     if (joint_name == "pan"):
